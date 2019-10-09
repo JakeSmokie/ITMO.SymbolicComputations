84e6bcb9bfdcd0336a9140d514419e94b4a29ac8
JakeSmokie <jakesmokie@gmail.com>
2019-10-10 01:46:40 +0300

    Add ExpressionInfo.AsJson() method

---
+++ ITMO.SymbolicComputations.Base/Tools/JsonExpressionPrinter.cs
@@ -0,0 +1,9 @@
+﻿using ITMO.SymbolicComputations.Base.Models;
+
+namespace ITMO.SymbolicComputations.Base.Tools {
+    public static class JsonExpressionPrinter {
+        public static string AsJson(this ExpressionInfo expressionInfo) {
+            return "";
+        }  
+    }
+}
