a7ed3b61600a3074f36884cc468fcf0c6dfa43ce
JakeSmokie <jakesmokie@gmail.com>
2019-10-10 01:34:16 +0300

    Implement simple read xml test

--- ITMO.SymbolicComputations.Base.Tests/Tests.cs
+++ ITMO.SymbolicComputations.Base.Tests/Tests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Xml;
+using ITMO.SymbolicComputations.Base.Models;
 using ITMO.SymbolicComputations.Base.Tools;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,6 +20,26 @@ namespace ITMO.SymbolicComputations.Base.Tests {
             document.Load("Samples/First.xml");
 
             var expressionInfo = XmlExpressionReader.ReadXml(document);
+            
+            var a = (BinaryOperation) expressionInfo.Expression;
+            var b = (BinaryOperation) a.First;
+            var c = (Symbol) a.Second;
+            var d = (BinaryOperation) b.First;
+            var e = (Constant) b.Second;
+            var f = (BinaryOperation) d.First;
+            var g = (Symbol) d.Second;
+            var h = (Constant) f.First;
+            var j = (Constant) f.Second;
+
+            Assert.Equal("Mul", a.Name);
+            Assert.Equal("Sum", b.Name);
+            Assert.Equal("x", c.Name);
+            Assert.Equal("Mul", d.Name);
+            Assert.Equal(10, e.Value);
+            Assert.Equal("Sum", f.Name);
+            Assert.Equal("x", g.Name);
+            Assert.Equal(3, h.Value);
+            Assert.Equal(5, j.Value);
         }
     }
 }
