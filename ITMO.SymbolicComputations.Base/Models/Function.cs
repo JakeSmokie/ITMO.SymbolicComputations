3d5b34b6f05d0c21ee38fd2047b4a18bd60f8705
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 01:19:56 +0300

    Add `Attributes` property in Function symbol

--- ITMO.SymbolicComputations.Base/Models/Function.cs
+++ ITMO.SymbolicComputations.Base/Models/Function.cs
@@ -1,13 +1,18 @@
 using System.Collections.Immutable;
+using Newtonsoft.Json;
 
 namespace ITMO.SymbolicComputations.Base.Models {
     public sealed class Function : BaseSymbol {
-        public Function(BaseSymbol symbol, ImmutableList<BaseSymbol> arguments) {
+        public Function(BaseSymbol symbol, ImmutableList<BaseSymbol> arguments, ImmutableHashSet<BaseSymbol> attributes = null) {
             Symbol = symbol;
             Arguments = arguments;
+            Attributes = attributes ?? ImmutableHashSet<BaseSymbol>.Empty;
         }
 
         public BaseSymbol Symbol { get; }
         public ImmutableList<BaseSymbol> Arguments { get; }
+        
+        [JsonIgnore]
+        public ImmutableHashSet<BaseSymbol> Attributes { get; }
     }
 }
