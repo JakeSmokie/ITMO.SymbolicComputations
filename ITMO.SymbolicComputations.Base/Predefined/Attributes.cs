ab76bd224f428821b7645e899565cbf3a33647e7
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 04:09:02 +0300

    Declare Flat attribute

--- ITMO.SymbolicComputations.Base/Predefined/Attributes.cs
+++ ITMO.SymbolicComputations.Base/Predefined/Attributes.cs
@@ -3,5 +3,6 @@
 namespace ITMO.SymbolicComputations.Base.Predefined {
     public static class Attributes {
         public static readonly StringSymbol HoldAll = new StringSymbol("HoldAll");
+        public static readonly StringSymbol Flat = new StringSymbol("Flat");
     }
 }
