0d0756ce06d6e91facfcf2330ccb8b08eb0e3015
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 04:10:24 +0300

    Declare OneIdentity attribute

--- ITMO.SymbolicComputations.Base/Predefined/Attributes.cs
+++ ITMO.SymbolicComputations.Base/Predefined/Attributes.cs
@@ -4,5 +4,6 @@ namespace ITMO.SymbolicComputations.Base.Predefined {
     public static class Attributes {
         public static readonly StringSymbol HoldAll = new StringSymbol("HoldAll");
         public static readonly StringSymbol Flat = new StringSymbol("Flat");
+        public static readonly StringSymbol OneIdentity = new StringSymbol("OneIdentity");
     }
 }
