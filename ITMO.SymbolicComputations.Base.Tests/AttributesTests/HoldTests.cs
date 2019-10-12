ee3f127c99bc23d84498692f8d1c4579b08dd693
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 19:53:01 +0300

    Test out HoldFirst

--- ITMO.SymbolicComputations.Base.Tests/AttributesTests/HoldTests.cs
+++ ITMO.SymbolicComputations.Base.Tests/AttributesTests/HoldTests.cs
@@ -1,4 +1,6 @@
-﻿using ITMO.SymbolicComputations.Base.Visitors;
+﻿using ITMO.SymbolicComputations.Base.Models;
+using ITMO.SymbolicComputations.Base.Predefined;
+using ITMO.SymbolicComputations.Base.Visitors;
 using ITMO.SymbolicComputations.Base.Visitors.Evaluation;
 using Xunit;
 using Xunit.Abstractions;
@@ -7,6 +9,9 @@ using static ITMO.SymbolicComputations.Base.Predefined.Functions;
 
 namespace ITMO.SymbolicComputations.Base.Tests.AttributesTests {
     public sealed class HoldTests {
+        private static readonly StringSymbol HoldFirst = new StringSymbol("", Attributes.HoldFirst);
+        private static readonly StringSymbol HoldRest = new StringSymbol("", Attributes.HoldRest);
+
         public HoldTests(ITestOutputHelper output) =>
             _out = output;
 
@@ -14,12 +19,24 @@ namespace ITMO.SymbolicComputations.Base.Tests.AttributesTests {
 
         [Fact]
         public void HoldFirstWorks() {
-            var source = HoldComplete[Evaluate[Plus[2]]];
+            var source = HoldFirst[Plus[2], Plus[3]];
             var expression = source.Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
-            Assert.Equal(source, expression);
+            Assert.Equal(HoldFirst[Plus[2], 3], expression);
         }
+
+        [Fact]
+        public void HoldFirstSuppressionWorks() {
+            var holdFirst = new StringSymbol("", Attributes.HoldFirst);
+
+            var source = holdFirst[Evaluate[Plus[2]], Plus[3]];
+            var expression = source.Visit(new FullEvaluator());
+
+            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
+            Assert.Equal(holdFirst[2, 3], expression);
+        }
+
 
         [Fact]
         public void HoldFormSuppressingWithEvaluateFunctionWorks() {
