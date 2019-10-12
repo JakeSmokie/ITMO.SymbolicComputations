877a1fe494d4bc58aa4e5eec80ac263a82aeb270
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 19:40:33 +0300

    Rearrange tests files

--- ITMO.SymbolicComputations.Base.Tests/AttributesTests/HoldTests.cs
+++ ITMO.SymbolicComputations.Base.Tests/AttributesTests/HoldTests.cs
@@ -5,34 +5,16 @@ using Xunit.Abstractions;
 using static ITMO.SymbolicComputations.Base.Predefined.ArithmeticFunctions;
 using static ITMO.SymbolicComputations.Base.Predefined.Functions;
 
-namespace ITMO.SymbolicComputations.Base.Tests {
-    public sealed class AttributesTests {
-        public AttributesTests(ITestOutputHelper output) =>
+namespace ITMO.SymbolicComputations.Base.Tests.AttributesTests {
+    public sealed class HoldTests {
+        public HoldTests(ITestOutputHelper output) =>
             _out = output;
 
         private readonly ITestOutputHelper _out;
-
-        [Fact]
-        public void ConstInsidePlusIsReducedToConst() {
-            var expression = Plus[Plus[Plus[Plus[2]]]]
-                .Visit(new FullEvaluator());
-            
-            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
-            Assert.Equal(2, expression);
-        }
-        
-        [Fact]
-        public void PlusInsidePlusIsReducedToOnePlus() {
-            var expression = Plus[Plus[Plus[Plus[2, 3]]]]
-                .Visit(new FullEvaluator());
-            
-            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
-            Assert.Equal(Plus[2, 3], expression);
-        }
         
         [Fact]
         public void HoldWorks() {
-            var source = Hold[Plus[Plus[Plus[Plus[2]]]]];
+            var source = Hold[Plus[2]];
             var expression = source.Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
@@ -41,7 +23,7 @@ namespace ITMO.SymbolicComputations.Base.Tests {
 
         [Fact]
         public void HoldFormWorks() {
-            var source = Plus[Plus[Plus[Plus[2]]]];
+            var source = Plus[2];
             var expression = HoldForm[source].Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
@@ -50,8 +32,8 @@ namespace ITMO.SymbolicComputations.Base.Tests {
         
         [Fact]
         public void NestedHoldWorks() {
-            var insides = Hold[Plus[Plus[2]]];
-            var source = Plus[Plus[insides]];
+            var insides = Hold[Plus[2]];
+            var source = Plus[insides];
             var expression = source.Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
@@ -60,16 +42,25 @@ namespace ITMO.SymbolicComputations.Base.Tests {
 
         [Fact]
         public void HoldSuppressingWithEvaluateFunctionWorks() {
-            var source = Hold[Evaluate[Plus[Plus[Plus[Plus[2]]]]]];
+            var source = Hold[Evaluate[Plus[2]]];
             var expression = source.Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
             Assert.Equal(Hold[2], expression);
         }
-        
+
+        [Fact]
+        public void HoldFormSuppressingWithEvaluateFunctionWorks() {
+            var source = HoldForm[Evaluate[Plus[2]]];
+            var expression = source.Visit(new FullEvaluator());
+
+            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
+            Assert.Equal(2, expression);
+        }
+
         [Fact]
         public void HoldIsNotSuppressedWhenItIsComplete() {
-            var source = HoldComplete[Evaluate[Plus[Plus[Plus[Plus[2]]]]]];
+            var source = HoldComplete[Evaluate[Plus[2]]];
             var expression = source.Visit(new FullEvaluator());
 
             _out.WriteLine(expression.Visit(new MathematicaPrinter()));
---
+++ ITMO.SymbolicComputations.Base.Tests/AttributesTests/OneIdentityTests.cs
@@ -0,0 +1,32 @@
+﻿using ITMO.SymbolicComputations.Base.Visitors;
+using ITMO.SymbolicComputations.Base.Visitors.Evaluation;
+using Xunit;
+using Xunit.Abstractions;
+using static ITMO.SymbolicComputations.Base.Predefined.ArithmeticFunctions;
+
+namespace ITMO.SymbolicComputations.Base.Tests.AttributesTests {
+    public sealed class OneIdentityTests {
+        public OneIdentityTests(ITestOutputHelper output) =>
+            _out = output;
+
+        private readonly ITestOutputHelper _out;
+
+        [Fact]
+        public void ConstInsidePlusIsReducedToConst() {
+            var expression = Plus[Plus[Plus[Plus[2]]]]
+                .Visit(new FullEvaluator());
+
+            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
+            Assert.Equal(2, expression);
+        }
+
+        [Fact]
+        public void PlusInsidePlusIsReducedToOnePlus() {
+            var expression = Plus[Plus[Plus[Plus[2, 3]]]]
+                .Visit(new FullEvaluator());
+
+            _out.WriteLine(expression.Visit(new MathematicaPrinter()));
+            Assert.Equal(Plus[2, 3], expression);
+        }
+    }
+}
