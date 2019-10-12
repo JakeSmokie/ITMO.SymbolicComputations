66b2b41b56029583030ca57ae0eb545de2aada77
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 05:19:58 +0300

    Revert "Implement Constant.Equals"
    This reverts commit 2e89febd

--- ITMO.SymbolicComputations.Base/Models/Constant.cs
+++ ITMO.SymbolicComputations.Base/Models/Constant.cs
@@ -1,8 +1,7 @@
-using System;
 using ITMO.SymbolicComputations.Base.Visitors;
 
 namespace ITMO.SymbolicComputations.Base.Models {
-    public sealed class Constant : Symbol, IEquatable<Constant> {
+    public sealed class Constant : Symbol {
         public Constant(decimal value) =>
             Value = value;
 
@@ -10,25 +9,5 @@ namespace ITMO.SymbolicComputations.Base.Models {
 
         protected override T VisitImplementation<T>(ISymbolVisitor<T> visitor) =>
             visitor.VisitConstant(this);
-
-        public bool Equals(Constant other) {
-            if (ReferenceEquals(null, other)) {
-                return false;
-            }
-
-            if (ReferenceEquals(this, other)) {
-                return true;
-            }
-
-            return Value == other.Value;
-        }
-
-        public override bool Equals(object obj) => ReferenceEquals(this, obj) || obj is Constant other && Equals(other);
-
-        public override int GetHashCode() => Value.GetHashCode();
-
-        public static bool operator ==(Constant left, Constant right) => Equals(left, right);
-
-        public static bool operator !=(Constant left, Constant right) => !Equals(left, right);
     }
 }
