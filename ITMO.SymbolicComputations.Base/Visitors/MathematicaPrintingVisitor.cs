faf28db11f73797b97fd0062e3d08578ca6d81c2
JakeSmokie <jakesmokie@gmail.com>
2019-10-12 03:42:17 +0300

    Make MathematicaPrintingVisitor sealed

--- ITMO.SymbolicComputations.Base/Visitors/MathematicaPrintingVisitor.cs
+++ ITMO.SymbolicComputations.Base/Visitors/MathematicaPrintingVisitor.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using ITMO.SymbolicComputations.Base.Models;
 
 namespace ITMO.SymbolicComputations.Base.Visitors {
-    public class MathematicaPrintingVisitor : ISymbolVisitor<string> {
+    public sealed class MathematicaPrintingVisitor : ISymbolVisitor<string> {
         public string VisitFunction(Function function) =>
             function.Symbol.Visit(this) +
             $"[{string.Join(", ", function.Arguments.Select(a => a.Visit(this)))}]";
