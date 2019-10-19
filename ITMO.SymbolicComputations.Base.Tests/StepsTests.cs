17c5b3c8e38faeff3a56fe7e4346e7766ef57a76
JakeSmokie <jakesmokie@gmail.com>
2019-10-19 23:04:36 +0300

    Rename step test

--- ITMO.SymbolicComputations.Base.Tests/StepsTests.cs
+++ ITMO.SymbolicComputations.Base.Tests/StepsTests.cs
@@ -13,14 +13,13 @@ namespace ITMO.SymbolicComputations.Base.Tests {
         private readonly ITestOutputHelper _out;
         private static readonly StringSymbol Orderless = new StringSymbol("Orderless", Attributes.Orderless);
 
-        
+
         [Fact]
-        public void NestedOrderingWorks() {
+        public void NestedOrderingLoggingWorks() {
             var source = Orderless["y", Orderless[1, "x", Orderless["y"]], "x", "z"];
             var steps = source.Visit(new FullEvaluator()).Steps;
 
             steps.ForEach(e => _out.WriteLine(e.Visit(new MathematicaPrinter())));
         }
-
     }
 }
