72a4689f7868f626b7ff5e4c2178de98a9f90317
JakeSmokie <jakesmokie@gmail.com>
2019-10-21 01:34:23 +0300

    Seal Program class

--- ITMO.SymbolicComputations.Base/Program.cs
+++ ITMO.SymbolicComputations.Base/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace ITMO.SymbolicComputations.Base {
-    internal class Program {
+    internal sealed class Program {
         private static void Main(string[] args) {
             Console.WriteLine("Ya tut igrayu!");
         }
