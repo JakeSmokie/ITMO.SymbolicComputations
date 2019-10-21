953e4284dbf2c6889219b3715b94a608d12bcd2a
JakeSmokie <jakesmokie@gmail.com>
2019-10-21 04:02:08 +0300

    Implement unique elements counting in GroupByExtensions

---
+++ ITMO.SymbolicComputations.Base/Tools/GroupByExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace ITMO.SymbolicComputations.Base.Tools {
+    public static class GroupByExtensions {
+        public static ImmutableList<(T Item, int Count)> CountUp<T>(this IEnumerable<T> enumerable) {
+            return enumerable.Aggregate(
+                ImmutableList<(T Item, int Count)>.Empty,
+                (list, t) => {
+                    var i = list.FindIndex(x => Equals(x.Item1, t));
+
+                    return i != -1
+                        ? list.SetItem(i, (list[i].Item, list[i].Count + 1))
+                        : list.Add((t, 1));
+                }
+            );
+        }
+    }
+}
