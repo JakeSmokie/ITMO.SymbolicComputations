e1b758099818b72d861031c457575c131661854f
JakeSmokie <jakesmokie@gmail.com>
2019-11-04 19:25:38 +0500

    Fix line separators issue

--- ITMO.SymbolicComputations.Base/Tools/JsonPrintingExtensions.cs
+++ ITMO.SymbolicComputations.Base/Tools/JsonPrintingExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using ITMO.SymbolicComputations.Base.Models;
 using Newtonsoft.Json;
 
-namespace ITMO.SymbolicComputations.Base.Tools {
-    public static class JsonPrintingExtensions {
+namespace ITMO.SymbolicComputations.Base.Tools
+{
+    public static class JsonPrintingExtensions
+    {
         public static string AsJson(this ExpressionInfo expressionInfo) =>
-            JsonConvert.SerializeObject(expressionInfo, Formatting.Indented) + "\r\n";
+            JsonConvert.SerializeObject(expressionInfo, Formatting.Indented) + "\n";
     }
 }
