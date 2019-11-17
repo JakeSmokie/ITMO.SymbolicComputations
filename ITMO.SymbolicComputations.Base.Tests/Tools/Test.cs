af2ca83f33ebab5e62f369b96b25179b35c51c60
JakeSmokie <jakesmokie@gmail.com>
2019-11-17 18:32:12 +0300

    Return back usual version of EvaluateAndAssert

--- ITMO.SymbolicComputations.Base.Tests/Tools/Test.cs
+++ ITMO.SymbolicComputations.Base.Tests/Tools/Test.cs
@@ -6,6 +6,13 @@ using Xunit.Abstractions;
 
 namespace ITMO.SymbolicComputations.Base.Tests.Tools {
     public static class Test {
+        public static void EvaluateAndAssert(Expression expression, Symbol expectedResult, ITestOutputHelper output) {
+            var (steps, actual) = expression.Visit(new FullEvaluator());
+            
+            steps.Print(output);
+            Assert.Equal(expectedResult, actual);
+        }
+        
         public static Action<Expression, Symbol> EvaluateAndAssert(ITestOutputHelper output) =>
             (expression, expected) => {
                 var (steps, actual) = expression.Visit(new FullEvaluator());
