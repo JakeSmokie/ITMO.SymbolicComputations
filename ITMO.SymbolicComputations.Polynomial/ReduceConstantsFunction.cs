9a89c71cadfbc8ce7031282dd6d9d763c6b8bb54
JakeSmokie <jakesmokie@gmail.com>
2019-11-17 21:26:14 +0300

    Implement ReduceConstantsFunction

---
+++ ITMO.SymbolicComputations.Polynomial/ReduceConstantsFunction.cs
@@ -0,0 +1,26 @@
+using ITMO.SymbolicComputations.Base.Models;
+using static ITMO.SymbolicComputations.Base.Predefined.Alphabet;
+using static ITMO.SymbolicComputations.Base.Predefined.ArithmeticFunctions;
+using static ITMO.SymbolicComputations.Base.Predefined.BooleanFunctions;
+using static ITMO.SymbolicComputations.Base.Predefined.CastingFunctions;
+using static ITMO.SymbolicComputations.Base.Predefined.Functions;
+using static ITMO.SymbolicComputations.Base.Predefined.ListFunctions;
+
+namespace ITMO.SymbolicComputations.Polynomial {
+    public static class ReduceConstantsFunction {
+        public static readonly Expression ReduceConstants =
+            Fun[list,
+                Fun["constants",
+                    If[
+                        Eq[Length["constants"], 0],
+                        list,
+                        Append[
+                            Filter[list, Fun[x, Not[IsConstant[x]]]],
+                            ListPlus["constants"]
+                        ],
+                        "Error"
+                    ]
+                ][Filter[list, Fun[x, IsConstant[x]]]]
+            ];
+    }
+}
