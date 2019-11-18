42d4dae07e01aa360095d6198453cc02d99e9310
savamas <savamas47@mail.ru>
2019-11-18 14:55:53 +0300

    Implement TimesConstantsTests

--- ITMO.SymbolicComputations.Polynomial.Tests/ConstantsTimesTests.cs
+++ ITMO.SymbolicComputations.Polynomial.Tests/ConstantsTimesTests.cs
@@ -15,7 +15,7 @@ namespace ITMO.SymbolicComputations.Polynomial.Tests {
         private readonly Action<Expression, Symbol> _evaluateAndAssert;
 
         [Fact]
-        public void ConstantsMultiplied() {
+        public void ConstantsMultipliedGeneral() {
             Symbol x = "x";
             Symbol y = "y";
             
@@ -24,6 +24,32 @@ namespace ITMO.SymbolicComputations.Polynomial.Tests {
                     List[3, x, y, 10, -1]
                 ],
                 List[x, y, -30]
+            );
+        }
+        
+        [Fact]
+        public void ConstantsMultipliedWithZero() {
+            Symbol x = "x";
+            Symbol y = "y";
+            
+            _evaluateAndAssert(
+                TimesConstants[
+                    List[3, x, y, 0, -1]
+                ],
+                0
+            );
+        }
+        
+        [Fact]
+        public void ConstantsMultipliedWithOne() {
+            Symbol x = "x";
+            Symbol y = "y";
+            
+            _evaluateAndAssert(
+                TimesConstants[
+                    List[1, x, y, 0.2m, 5]
+                ],
+                List[x, y]
             );
         }
     }
