a4f1854de349158fbf74ae379610708b5583567e
savamas <savamas47@mail.ru>
2019-11-18 14:47:46 +0300

    Implement TimesConstants

---
+++ ITMO.SymbolicComputations.Polynomial.Tests/ConstantsTimesTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ITMO.SymbolicComputations.Base.Models;
+using Tests.Base.Tools;
+using Xunit;
+using Xunit.Abstractions;
+using static ITMO.SymbolicComputations.Base.Functions.ListFunctions;
+using static ITMO.SymbolicComputations.Polynomial.TimesConstantsFunction;
+
+namespace ITMO.SymbolicComputations.Polynomial.Tests {
+    public class ConstantsTimesTests {
+        public ConstantsTimesTests(ITestOutputHelper output) {
+            _evaluateAndAssert = Test.CreateAsserter(output);
+        }
+
+        private readonly Action<Expression, Symbol> _evaluateAndAssert;
+
+        [Fact]
+        public void ConstantsMultiplied() {
+            Symbol x = "x";
+            Symbol y = "y";
+            
+            _evaluateAndAssert(
+                TimesConstants[
+                    List[3, x, y, 10, -1]
+                ],
+                List[x, y, -30]
+            );
+        }
+    }
+}
---
+++ ITMO.SymbolicComputations.Polynomial/TimesConstantsFunction.cs
@@ -0,0 +1,39 @@
+﻿using ITMO.SymbolicComputations.Base.Models;
+using static ITMO.SymbolicComputations.Base.Functions.Alphabet;
+using static ITMO.SymbolicComputations.Base.Functions.ArithmeticFunctions;
+using static ITMO.SymbolicComputations.Base.Functions.BooleanFunctions;
+using static ITMO.SymbolicComputations.Base.Functions.CastingFunctions;
+using static ITMO.SymbolicComputations.Base.Functions.Functions;
+using static ITMO.SymbolicComputations.Base.Functions.ListFunctions;
+
+namespace ITMO.SymbolicComputations.Polynomial {
+    public static class TimesConstantsFunction {
+        public static readonly Expression TimesConstants =
+            Fun[list,
+                Fun["constants", Fun["others",
+                    If[
+                        Eq[Length["constants"], 0],
+                        //
+                        list,
+                        Evaluate[If[
+                            Eq[ListTimes["constants"], 0],
+                            //
+                            0,
+                            Evaluate[If[
+                                Eq[ListTimes["constants"], 1],
+                                "others",
+                                Evaluate[Append[
+                                    "others",
+                                    ListTimes["constants"]
+                                ]]
+                            ]]
+                        ]]
+                    ]
+                ]][
+                    Filter[list][Fun[x, IsConstant[x]]]
+                ][
+                    Filter[list][Fun[x, Not[IsConstant[x]]]]
+                ]
+            ];
+    }
+}
