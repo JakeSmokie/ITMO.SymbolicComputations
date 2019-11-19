8bdf1c59bb58ecf7adb581f1c2bb1663590fabe9
JakeSmokie <jakesmokie@gmail.com>
2019-11-19 11:56:19 +0300

    Remove power

--- ITMO.SymbolicComputations.Base.Tests/ImplementationsTests/PowerTests.cs
+++ ITMO.SymbolicComputations.Base.Tests/ImplementationsTests/PowerTests.cs
@@ -1,42 +1,42 @@
-using System;
-using ITMO.SymbolicComputations.Base.Models;
-using Tests.Base.Tools;
-using Xunit;
-using Xunit.Abstractions;
-using static ITMO.SymbolicComputations.Base.StandardLibrary.ArithmeticFunctions;
-
-namespace ITMO.SymbolicComputations.Base.Tests.ImplementationsTests {
-    public class PowerTests {
-        public PowerTests(ITestOutputHelper output) {
-            _evaluateAndAssert = Test.CreateAsserter(output);
-        }
-
-        private readonly Action<Expression, Symbol> _evaluateAndAssert;
-
-        [Fact]
-        public void NotPowerIsNotProcessed() {
-            _evaluateAndAssert(
-                PowerImplementation[Plus["x", "y"]],
-                Plus["x", "y"]
-            );
-
-            _evaluateAndAssert(
-                PowerImplementation[Times["x", "y"]],
-                Times["x", "y"]
-            );
-        }
-
-        [Fact]
-        public void PowerWithSymbolsIsNotProcessed() {
-            _evaluateAndAssert(
-                PowerImplementation[Power["x", "y"]],
-                Power["x", "y"]
-            );
-
-            _evaluateAndAssert(
-                PowerImplementation[Power["x", 2]],
-                Power["x", 2]
-            );
-        }
-    }
-}
+//using System;
+//using ITMO.SymbolicComputations.Base.Models;
+//using Tests.Base.Tools;
+//using Xunit;
+//using Xunit.Abstractions;
+//using static ITMO.SymbolicComputations.Base.StandardLibrary.ArithmeticFunctions;
+//
+//namespace ITMO.SymbolicComputations.Base.Tests.ImplementationsTests {
+//    public class PowerTests {
+//        public PowerTests(ITestOutputHelper output) {
+//            _evaluateAndAssert = Test.CreateAsserter(output);
+//        }
+//
+//        private readonly Action<Expression, Symbol> _evaluateAndAssert;
+//
+//        [Fact]
+//        public void NotPowerIsNotProcessed() {
+//            _evaluateAndAssert(
+//                PowerImplementation[Plus["x", "y"]],
+//                Plus["x", "y"]
+//            );
+//
+//            _evaluateAndAssert(
+//                PowerImplementation[Times["x", "y"]],
+//                Times["x", "y"]
+//            );
+//        }
+//
+//        [Fact]
+//        public void PowerWithSymbolsIsNotProcessed() {
+//            _evaluateAndAssert(
+//                PowerImplementation[Power["x", "y"]],
+//                Power["x", "y"]
+//            );
+//
+//            _evaluateAndAssert(
+//                PowerImplementation[Power["x", 2]],
+//                Power["x", 2]
+//            );
+//        }
+//    }
+//}
